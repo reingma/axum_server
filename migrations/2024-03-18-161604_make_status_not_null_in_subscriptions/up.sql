93db13236b1f8f1d75a450558b5886e7e1e8b4ee
reingma <gabriel.masarin.aguiar@gmail.com>
2024-03-20 13:14:34 +0100

    Removing transaction from migration

--- migrations/2024-03-18-161604_make_status_not_null_in_subscriptions/up.sql
+++ migrations/2024-03-18-161604_make_status_not_null_in_subscriptions/up.sql
@@ -1,7 +1,5 @@
 -- Your SQL goes here
-BEGIN;
     UPDATE subscriptions
         SET status = 'confirmed' 
         WHERE status IS NULL;
     ALTER TABLE subscriptions ALTER COLUMN status SET NOT NULL;
-COMMIT;
