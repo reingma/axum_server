96c1abe2a13bed9ca2e4f6b6dc2915ffe910e937
reingma <gabriel.masarin.aguiar@gmail.com>
2024-03-20 03:09:40 +0100

    Diesel transaction

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -4,6 +4,8 @@ use crate::{
     startup::ApplicationState,
 };
 use axum::{extract::State, http::StatusCode, Form};
+use diesel_async::scoped_futures::ScopedFutureExt;
+use diesel_async::AsyncConnection;
 use rand::distributions::Alphanumeric;
 use rand::thread_rng;
 use rand::Rng;
@@ -35,32 +37,29 @@ pub async fn subscriptions(
     let mut connection =
         crate::database::get_connection(app_state.database_pool).await;
 
-    let subscriber_id =
-        match insert_subscriber(&mut connection, &new_subscriber).await {
-            Ok(subscriber_id) => subscriber_id,
-            Err(_) => return StatusCode::INTERNAL_SERVER_ERROR,
-        };
     let subscription_token = generate_subscription_token();
+    let copy = subscription_token.clone();
+    if connection
+        .transaction::<_, diesel::result::Error, _>(|conn| {
+            async move {
+                let subscriber_id =
+                    insert_subscriber(conn, &new_subscriber).await?;
 
-    if queries::store_token(
-        &mut connection,
-        &subscription_token,
-        &subscriber_id,
-    )
-    .await
-    .is_err()
-    {
-        return StatusCode::INTERNAL_SERVER_ERROR;
-    }
-
-    if send_confirmation_email(
-        &app_state.email_client,
-        new_subscriber,
-        &app_state.base_url,
-        &subscription_token,
-    )
-    .await
-    .is_err()
+                queries::store_token(conn, &copy, &subscriber_id).await?;
+                send_confirmation_email(
+                    &app_state.email_client,
+                    new_subscriber,
+                    &app_state.base_url,
+                    &subscription_token,
+                )
+                .await
+                .map_err(|_| diesel::result::Error::RollbackTransaction)?;
+                Ok(())
+            }
+            .scope_boxed()
+        })
+        .await
+        .is_err()
     {
         return StatusCode::INTERNAL_SERVER_ERROR;
     }
